a01cb3992e4830c7869d6a9ee052fa2789a9ce25
Muhammad Anns Khan <140887479+maybeanns@users.noreply.github.com>
2024-01-04 22:38:34 +0500

    Conditional,etc

---
+++ conditional.py
@@ -0,0 +1,32 @@
+# conditional 
+X =10
+if X > 10:
+    print("greater than ten")
+elif X < 10:
+    print("less than ten")
+else:
+    print("its 10")
+
+# loops
+    for i in range (10):
+        if i ==5:
+            continue
+        if i==8:
+            break
+        print(i)
+
+# nested loops
+        for i in range(3):
+            for j in range(2):
+                print(f"({i}, {j})")
+                
+# while loops
+    count =0
+    while count <5:
+        print(count)
+        count+=1                
+    
+    
+    
+
+    
