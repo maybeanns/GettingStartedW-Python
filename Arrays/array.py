a717a58f1fe975ef00ebbc4e73fba9c87b0b8ef0
Muhammad Anns Khan <m1annskhan@gmail.com>
2025-06-06 22:26:01 +0500

    arrays

---
+++ Arrays/array.py
@@ -0,0 +1,18 @@
+#collections are : Lists, Tuples, Sets, Dictionaries
+
+# Lists are mutable, ordered collections of items that can be of any data type, including strings, integers, floats, and other lists.
+# Tuples are immutable, ordered collections of items that can be of any data type, including strings, integers, floats, and other tuples.
+# Sets are unordered collections of unique items that can be of any data type, including strings, integers, floats, and other sets.
+# Dictionaries are unordered collections of key-value pairs, where each key is unique and maps to a value that can be of any data type, including strings, integers, floats, and other dictionaries.
+# Immutable collections are collections that cannot be changed after they are created. This means that you cannot add, remove, or modify items in an immutable collection. Examples of immutable collections in Python include tuples and frozensets.
+# Mutable collections are collections that can be changed after they are created. This means that you can add, remove, or modify items in a mutable collection. Examples of mutable collections in Python include lists, sets, and dictionaries.
+
+# Lists
+# fruits = ["apple", "banana", "cherry"]
+# nums = [1, 2, 3, 4, 5]
+# #print(fruits) 
+# # for x in nums:
+#     # y = x[::-1]
+# rev_nums = nums[::-1]
+# print(rev_nums)
+
